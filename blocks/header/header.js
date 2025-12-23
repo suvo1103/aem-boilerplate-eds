41694c23f835dc7d93a817244ae4885c186c90e1
oneds\suvo.banerjee <suvo.banerjee>
2025-12-23 23:58:52 +0530

    adding usds related changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -108,7 +108,159 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-  // load nav as fragment
+  // Create overlay
+  const overlay = document.createElement('div');
+  overlay.className = 'usa-overlay';
+  block.append(overlay);
+
+  // Create header
+  const header = document.createElement('header');
+  header.className = 'usa-header usa-header--basic usa-header--megamenu';
+
+  // Nav container
+  const navContainer = document.createElement('div');
+  navContainer.className = 'usa-nav-container';
+
+  // Navbar
+  const navbar = document.createElement('div');
+  navbar.className = 'usa-navbar';
+
+  // Logo
+  const logo = document.createElement('div');
+  logo.className = 'usa-logo';
+  logo.innerHTML = `<em class="usa-logo__text"><a href="/" title="&lt;Project title&gt;">&lt;Project title&gt;</a></em>`;
+
+  // Menu button
+  const menuBtn = document.createElement('button');
+  menuBtn.type = 'button';
+  menuBtn.className = 'usa-menu-btn';
+  menuBtn.textContent = 'Menu';
+
+  navbar.append(logo, menuBtn);
+
+  // Navigation
+  const nav = document.createElement('nav');
+  nav.className = 'usa-nav';
+  nav.setAttribute('aria-label', 'Primary navigation');
+
+  // Close button
+  const closeBtn = document.createElement('button');
+  closeBtn.type = 'button';
+  closeBtn.className = 'usa-nav__close';
+  closeBtn.innerHTML = `<img src="/assets/img/usa-icons/close.svg" role="img" alt="Close" />`;
+
+  // Primary nav list
+  const navList = document.createElement('ul');
+  navList.className = 'usa-nav__primary usa-accordion';
+
+  // Example nav item with submenu
+  const navItem1 = document.createElement('li');
+  navItem1.className = 'usa-nav__primary-item';
+  navItem1.innerHTML = `
+    <button
+      type="button"
+      class="usa-accordion__button usa-nav__link usa-current"
+      aria-expanded="false"
+      aria-controls="basic-mega-nav-section-one"
+    >
+      <span>&lt;Current section&gt;</span>
+    </button>
+    <div
+      id="basic-mega-nav-section-one"
+      class="usa-nav__submenu usa-megamenu"
+    >
+      <div class="grid-row grid-gap-4">
+        ${[1,2,3,4].map(() => `
+        <div class="usa-col">
+          <ul class="usa-nav__submenu-list">
+            <li class="usa-nav__submenu-item">
+              <a href="javascript:void(0);">&lt;Navigation link&gt;</a>
+            </li>
+            <li class="usa-nav__submenu-item">
+              <a href="javascript:void(0);">&lt;Navigation link&gt;</a>
+            </li>
+            <li class="usa-nav__submenu-item">
+              <a href="javascript:void(0);">&lt;Navigation link&gt;</a>
+            </li>
+          </ul>
+        </div>
+        `).join('')}
+      </div>
+    </div>
+  `;
+
+  // Example nav item with submenu (second)
+  const navItem2 = document.createElement('li');
+  navItem2.className = 'usa-nav__primary-item';
+  navItem2.innerHTML = `
+    <button
+      type="button"
+      class="usa-accordion__button usa-nav__link"
+      aria-expanded="false"
+      aria-controls="basic-mega-nav-section-two"
+    >
+      <span>&lt;Section&gt;</span>
+    </button>
+    <div
+      id="basic-mega-nav-section-two"
+      class="usa-nav__submenu usa-megamenu"
+    >
+      <div class="grid-row grid-gap-4">
+        ${[1,2,3,4].map(() => `
+        <div class="usa-col">
+          <ul class="usa-nav__submenu-list">
+            <li class="usa-nav__submenu-item">
+              <a href="javascript:void(0);">&lt;Navigation link&gt;</a>
+            </li>
+            <li class="usa-nav__submenu-item">
+              <a href="javascript:void(0);">&lt;Navigation link&gt;</a>
+            </li>
+            <li class="usa-nav__submenu-item">
+              <a href="javascript:void(0);">&lt;Navigation link&gt;</a>
+            </li>
+          </ul>
+        </div>
+        `).join('')}
+      </div>
+    </div>
+  `;
+
+  // Simple nav link
+  const navItem3 = document.createElement('li');
+  navItem3.className = 'usa-nav__primary-item';
+  navItem3.innerHTML = `
+    <a href="javascript:void(0);" class="usa-nav-link">&lt;Simple link&gt;</a>
+  `;
+
+  navList.append(navItem1, navItem2, navItem3);
+
+  // Search section
+  const searchSection = document.createElement('section');
+  searchSection.setAttribute('aria-label', 'Search component');
+  searchSection.innerHTML = `
+    <form class="usa-search usa-search--small" role="search">
+      <label class="usa-sr-only" for="search-field">Search</label>
+      <input
+        class="usa-input"
+        id="search-field"
+        type="search"
+        name="search"
+      />
+      <button class="usa-button" type="submit">
+        <img
+          src="/assets/img/usa-icons-bg/search--white.svg"
+          class="usa-search__submit-icon"
+          alt="Search"
+        />
+      </button>
+    </form>
+  `;
+
+  nav.append(closeBtn, navList, searchSection);
+  navContainer.append(navbar, nav);
+  header.append(navContainer);
+  block.append(header);
+}
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(navPath);
